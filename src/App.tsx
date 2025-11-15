2b586f4a229b88aed994edd374a6d9cbd27bf38b
erladitia14 <erladitia14@upi.edu>
2025-11-15 17:58:11 +0700

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import './App.css'
-import Search from './components/Search'
-import WeatherInfo from './components/WeatherInfo'
 import WeatherApp from './components/WeatherApp';
 import About from './components/About'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
